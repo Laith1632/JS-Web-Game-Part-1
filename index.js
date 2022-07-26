d1458f8a370d3ab8bb1ff950387c225c96d39e74
Laith Albusairi <laithalbusairi@gmail.com>
2022-07-26 13:34:27 -0700

    redid code

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-function tile(url, left, bottom, width, height){
-    for(let h = 0; h < height; h++){
-        for(let w = 0; w < width; w++){
-            newImage(url, left + w*100, bottom+h*100)
-        }
-    }
+function newImage(imgs, left, bottom){
+    let image = document.createElement('img')
+    image.src = imgs;
+    image.style.position = 'fixed'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    document.body.append(image)
+    return image;
 }
-
-function newImage(url, left, bottom){
-    let object = document.createElement('img')
-    object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
-    document.body.append(object)
-    return object
-}
-
-function newItem(url, left, bottom){
-    let object = newImage(url, left, bottom)
-
-    object.addEventListener('dblclick', () => {
-        object.remove()
-    })
-}
-
-let horizon = window.innerHeight / 1.75
-let heightOfSky = window.innerHeight-horizon
-let heightOfGrass = horizon
-
-tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
-tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
-
 
 newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
-newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
-newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
+
+function newItem(imgs, left, bottom){
+    let item = newImage(imgs, left, bottom)
+    item.addEventListener('dblclick', function(){
+        item.remove()
+        let Weapons = document.createElement('img')
+        Weapons.src = imgs;
+        weapon.append(Weapons)
+    })
+}
+
+newItem('assets/sword.png', 500, 405);
+newItem('assets/sheild.png', 165, 185);
+newItem('assets/staff.png', 600, 100);
+newItem('assets/crate.png', 150, 200);
+
+
+function newGrass (imgs,left, bottom){
+    let image = document.createElement('img')
+    image.src = imgs;
+    image.style.position = 'fixed'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    document.body.append(image)
+    return image;
+ 
+}
